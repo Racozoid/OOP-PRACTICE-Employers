18f0e5e3e00d1250d17bb085c8ee6744b8b9f78c
Alexander Korkin <A.A.Korkin@urfu.me>
2021-09-11 03:15:09 +0500

    Переписал программу
    Немного (много) изменил программу

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
-from classes import Staff
+workers = dict()    # Словарь в котором хранятся работники
+
+
+def someSort():
+    # Сортировка списка по ФИО
+    global workers
+    list_workers = list(workers.items())
+    list_workers.sort(key= lambda i: i[1][0])
+    sworkers = dict()
+    j = 1
+    for i in list_workers:
+        sworkers[j] = workers[i[0]]
+        j += 1
+    workers = sworkers
+
 
 while True:
-    print('\nЧто вы хотите?\nСписок комманд:')
-    print('д -- добавить сотрудника\nу -- удалить сотрудника\nп -- показать данные последнего внесенного сотрудника')
-    print('к -- показать колличество сотрудников\nи -- изменить долность сотрудника')
-    command = input()
-    if command == 'д':
-        a = input('Введите ФИО: ')
-        b = input('Введите зарплату: ')
-        c = input('Введите должность: ')
+    print('\n\nСписок команд:\nq -- выход\nadd -- добавить сотрудника\nshow -- показать список сотрудников'
+          '\ndel -- удалить сотрудника')
+    command = input('Введите команду: ')
+    if command == 'q':
+        break
+        # Выход из программы
 
-        person = Staff(a, b, c)
-        print('Пользователь {} добавлен'.format(a))
-    elif command == 'к':
-        Staff.displayCount()
-    elif command == 'у':
-        print('Work in progress. Данная функция будет добалена в следующих версиях.')
-        person.__del__()
-    elif command == 'п':
-        if Staff.count < 1:
-            print('Некого показывать')
+    elif command == 'add':
+        # Добавит нового сотрудника в словарь с сотрудниками
+        name = input('\nВведите ФИО: ')
+        pos = input('Введите должность: ')
+        try:    # Проверка на неверные значения зарплаты
+            cash = int(input('Введите зарплату: '))
+        except ValueError:
+            print('Введено некорректное значение! Попробуйте снова')
             continue
         else:
-            person.displayInfo()
-    elif command == 'и':
-        print('Work in progress. Данная функция будет добалена в следующих версиях.')
-    else:
-        print('Неверная команда')
-    q = input('Хотите выйти? Если хотите, то напишите q\n--')
-    if q == 'q':
-        break
+            if cash < 0:
+                # Проверка на отрицательные числа
+                print('Некорректная заработная плата. Попробуйте снова.')
+                continue
+            workers[len(workers) + 1] = (name, pos, cash)
+        someSort()  # Сортировка после добавления сотрудника
 
+    elif command == 'show':
+        # Вывод таблицы с работниками и их количества
+        if len(workers) != 0:   # Проверка на то есть ли работники
+            print('\n№ : личные данные')
+            for i in workers:
+                print(i, ':', workers[i][0], '\n\tДолжность:', workers[i][1], '\n\tЗарплата:', workers[i][2])
+        print('\nКоличество сотрудников:', len(workers))
 
-
-
+    elif command == 'del':
+        # Удаление работника из словаря
+        d = int(input('\nВведите № сотрудника для удаления: '))
+        del workers[d]
+        someSort()  # Сортировка после удаления
